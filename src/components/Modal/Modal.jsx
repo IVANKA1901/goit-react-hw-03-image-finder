3a4ee03d03d381711f46c4d0c5e9e50785766a3f
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-05-29 23:19:10 +0200

    complete task

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './Modal.module.css';
+
+export class Modal extends Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.handlePressEsc);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.handlePressEsc);
+  }
+
+  handlePressEsc = e => {
+    if (e.code === 'Escape') this.props.onCloseModal();
+  };
+
+  handleCloseModal = e => {
+    if (e.target === e.currentTarget && e.code === 'Escape') {
+      this.props.onCloseModal();
+    }
+  };
+
+  render() {
+    return (
+      <div className={css.overlay} onClick={this.handleCloseModal}>
+        <div className={css.modal}>{this.props.children} </div>
+      </div>
+    );
+  }
+}
+
+Modal.propTypes = {
+  onCloseModal: PropTypes.func.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
